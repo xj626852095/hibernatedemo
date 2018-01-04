1375ee314b9a9608a914137bf94a5e96cbf434fc
kevin.xiang <xiangjie012455@163.com>
2018-01-04 23:00:26 +0800

    add db sql

---
+++ src/main/resources/db.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `employee` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(32) NOT NULL DEFAULT '',
+  `lastName` varchar(32) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8;
